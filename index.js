45d9f6ccb7075f472e92041a37a3e66e998f0d4f
Ghous Ahmed <ghous.shah91@gmail.com>
2024-05-06 22:39:00 +0500

    fixed port

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
 
 app.use("/api", routes);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = ENV.port || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
